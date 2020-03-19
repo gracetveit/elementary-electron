0be1ec507f4947b9d460583475c3afb361f7f8fe
Grace Tveit <grace.tveit@gmail.com>
2020-03-19 13:09:03 -0700

    added comments to app.js

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ var electron = require('electron')
 
 electron.app.on('ready', function () {
     var mainWindow = new electron.BrowserWindow({width:600, height: 800})
+    // Creates a mainwondow with a fixed width and height
     mainWindow.loadURL('file://' + __dirname + '/index.html')
+    //Indside the window, loads a url with index.html.
 })
