69b38fea9a9d923b5b40811dea9e8ef736193dd2
Grace Tveit <grace.tveit@gmail.com>
2020-03-19 13:22:48 -0700

    Completed third(?) challenge

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-require('cat-picture')
+var picture = require('cat-picture')
+var image = require('lightning-image-poly')
+var src = picture.src
+picture.remove()
+var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'})
